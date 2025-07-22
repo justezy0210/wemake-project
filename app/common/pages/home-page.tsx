14a9f9817d5d3e6e8e1aad5746f8715338c397a9
2bogo <justezy0210@gmail.com>
2025-07-22 19:38:18 +0900

    cursor

---
+++ app/common/pages/home-page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <h1 className="text-4xl font-bold mb-6">WeMake</h1>
+    </div>
+  );
+}
--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,5 @@
 import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
-export default [index('routes/home.tsx')] satisfies RouteConfig;
+export default [index('common/pages/home-page.tsx')] satisfies RouteConfig;
 
 // routes.ts 파일은 사용자 URL의 위치에 따라 렌더링할 내용을 정의해서  React 라우터에 알려준다.
